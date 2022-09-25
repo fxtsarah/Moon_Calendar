9a2e115fc994615d384ee8bba01c85158596f4d4
fxtsarah <91339857+fxtsarah@users.noreply.github.com>
2022-09-25 17:41:26 -0400

    add events works as intended'

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, url_for, request
 from datetime import datetime
 from moonphase import position, phase
@@ -11,6 +12,7 @@ app = Flask(__name__)
 
 # start sql query functions
 
+# send a sql query to the database
 def query(query_text):
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
@@ -29,7 +31,7 @@ def query(query_text):
 
     conn.close()
     return dicts
-
+# send a sql query with additional arguments to the database
 def mod_query(query_text, *args):
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
@@ -37,15 +39,21 @@ def mod_query(query_text, *args):
     conn.commit()
     conn.close()
 
+# get a list of all the events saved in the database
 def get_all_events():
     return query("SELECT * FROM events")
 
+# get a list of all the equinoxes and solstices saved in the database
 def get_all_seasons():
     return query("SELECT * FROM seasons")
 
+# get a list of all the static day events that corresplond to a particular month and day in the database
+# static day events are holidays that always fall on the same day every year
 def get_static_day_events(mon, day):
     return query("SELECT NAME FROM events where month = " + str(mon) + " and day = " + str(day))
 
+# get a list of all the varied day events that corresplond to a particular month, day, and year
+# varied day events are holidays that fall on the nth particular weekday of a month every year.
 def get_varied_day_events(mon, day, year):
     cur_numweekday = datetime(year, mon, day).weekday()
     cur_weekday_of_mon = ceil(day / 7)
@@ -53,24 +61,35 @@ def get_varied_day_events(mon, day, year):
     + " and weekday = " + str(cur_numweekday) 
     + " and weekdayofmonth = " + str(cur_weekday_of_mon))
 
+# get a list of all the equinoxes and solstices that correspond to a particular month, day, and year.
 def get_day_seasons(mon, day, year):
     return query("SELECT NAME FROM seasons where year = " + str(year) + " and month = " + str(mon) + " and day = " + str(day))
 
+# get all of the events, equinoxes, and solstices that correspond to a particular month, day, and year.
 def get_day_events(mon, day, year):
     static_days = get_static_day_events(mon, day)
     varied_days = get_varied_day_events(mon, day, year)
     seasons = get_day_seasons(mon, day, year)
     return static_days + varied_days + seasons
 
+# add a static event to the database
+# static day events are holidays that always fall on the same day every year
 def add_static_event(name, mon, day):
-    return mod_query("INSERT INTO events (name, month, day) VALUES (?,?,?)", name, mon, day)
+    # make sure there is not already an event of this name:
+    if len(query("SELECT * FROM events where name = \"" + name + "\"")) == 0:
+        return mod_query("INSERT INTO events (name, month, day) VALUES (?,?,?)", name, mon, day)
 
+#add a varied day event to the database. 
+# varied day events are holidays that fall on the nth particular weekday of a month every year.
 def add_varied_day_event(name, mon, weekday, weekdayofmonth):
-    return mod_query("INSERT INTO events (name, month, weekday, weekdayofmonth) VALUES (?,?,?,?)", name, mon, weekday, weekdayofmonth)
+    # make sure there is not already an event of this name:
+    if len(query("SELECT * FROM events where name = \"" + name + "\"")) == 0:
+        return mod_query("INSERT INTO events (name, month, weekday, weekdayofmonth) VALUES (?,?,?,?)", name, mon, weekday, weekdayofmonth)
 
 # end sql query functions
 # start calendar calculation functions
 
+# finds the image that shows the right moon phase for a particular lunar position.
 @app.template_filter('find_moon_image')
 def find_moon_image(pos):
     index = (pos * dec(8)) + dec("0.5")
@@ -86,14 +105,16 @@ def find_moon_image(pos):
       7: "waningCrescent.png" } [int(index) & 7]
     return url_for('static', filename = filename)
 
+# finds the current date.
 now = datetime.now()
 
 # end calendar calculation functions
 # start render html funtions
 
-
 @app.route("/", methods=["GET", "POST"])
 def start():
+    # for event in get_all_events():
+    #     print(json.dumps(event) + "\n")
     wk_start_day = 1
 
     year = now.year
@@ -104,85 +125,31 @@ def start():
         
         d = request.form.to_dict()
         print(d)
+
         if len(d) != 4:
                     if d["event_type"] == "static_day":
                         add_static_event(d["new_event_name"], d["new_event_month"], d["new_event_day"])
                     elif d["event_type"] == "varied_day":
-                        add_varied_day_event(d["new_event_name"], d["new_event_month"], d["new_event_weekday"], d["new_event_weekdayofmonth"])
+                        add_varied_day_event(d["new_event_name"], d["new_event_month_varied"], d["new_event_weekday"], d["new_event_weekdayofmonth"])            
         else:
             year = int(d["year"])
             month = int(d["month"])
             selected_day = int(d["day"])
             wk_start_day = int(d["wk_start_day"])
 
-
-
-    # if month == 12:
-    #     nextmonth = 1
-    #     prevmonth = 11
-    #     nextyear = year + 1
-    #     prevyear = year
-    
-    # elif month == 1:
-    #     nextmonth = 2
-    #     prevmonth = 12
-    #     nextyear = year
-    #     prevyear = year - 1
-        
-    # else:
-    #     nextmonth = month + 1
-    #     prevmonth = month - 1
-    #     nextyear = year
-    #     prevyear = year
-    
-    week_sunstart = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    week_monstart = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    # monthname = datetime(year, month, 1).strftime("%B")
-    numweekday = datetime(year, month, 1).weekday()
-
-    if wk_start_day == 0:
-        placeholders = list(range(0, numweekday))
-    else:
-        placeholders = list(range(0, (numweekday + 1) % 7))
-  
-
-    #selected_day_datetime = datetime(year, month, selected_day, 0, 0)
-
     dates = []
     lastofmonth = calendar.monthrange(year, month)[1]
-
-    #event_list = get_day_events(month, selected_day, year)
 
     for i in range(1, (lastofmonth + 1)):
         day = datetime(year, month, i, 0, 0)
         dates.append((day, phase(position(day)), (position(day))))
 
-    return render_template('calendar.html'
-     , 
+    return render_template('calendar.html', 
     selected_day = selected_day, 
-    # selected_phase = phase(position(selected_day_datetime)),
-    # monthname = monthname, 
-     month = month,
-     year = year,
-     dates = dates, 
-    # week = week, 
-    # placeholders = placeholders)
-    # nextmonth = nextmonth,
-    # prevmonth = prevmonth,
-    # nextyear = nextyear,
-    # prevyear = prevyear,
-    # event_list = event_list,
-     wk_start_day = wk_start_day)
-
-@app.route("/add", methods=["GET","POST"])
-def start_add():
-    if request.method == "POST":
-        d = request.form.to_dict()
-        if d["event_type"] == "static_day":
-            add_static_event(d["new_event_name"], d["new_event_month"], d["new_event_day"])
-        elif d["event_type"] == "varied_day":
-            add_varied_day_event(d["new_event_name"], d["new_event_month"], d["new_event_weekday"], d["new_event_weekdayofmonth"])
-    return render_template('add-event-page.html') #do we need???
+    month = month,
+    year = year,
+    dates = dates, 
+    wk_start_day = wk_start_day)
 
 @app.route('/api/moon_img/<int:month>/<int:day>/<int:year>') 
 def moon_img_api(month, day, year):
@@ -199,7 +166,6 @@ def day_events_api(month, day, year):
     returnDict = {
         "key" : eventNames
     }
-
     return returnDict
 
 @app.route('/api/moon_imgs_month/<int:month>/<int:year>/<int:lastOfMonth>') 
@@ -208,8 +174,6 @@ def moon_imgs_month_api(month, year, lastOfMonth):
     for i in range (1, lastOfMonth + 1):
         moon_images[i] = moon_img_api(month, i, year)
     return moon_images
-
-    
 
 # end render html funtions
 
--- static/main.js
+++ static/main.js
@@ -1,15 +1,25 @@
 const week_sunstart = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const week_monstart = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 var selected_day;
+
+// loads the page for the monthly calendar
+// day is the day that is currently selected, and wk_start_day
+// is the day of the week the week starts on.
 function calendarPage(month, year, day, wk_start_day) {
     selected_day = day;
+
+    // fullDate is the date object that corresponds to the selected Day
     var fullDate = (
         new Date(year, month - 1, selected_day)
     )
-    var firstDayInMonthIndex 
+    
+    // lastDayInMonth is the day number the month ends on (28, 29, 30, or 31)
     var lastDayInMonth = (
         new Date(year, month, 0).getDate()
     )
+
+    // firstDayInMonthIndex is the weekday the month starts on
+    var firstDayInMonthIndex 
 
     if (wk_start_day == 0) {
         weekList = week_monstart
@@ -25,6 +35,8 @@ function calendarPage(month, year, day, wk_start_day) {
     }
 
 
+    // nextmonth and nextyear are the month and year that correspond to the following month on the calendar.
+    // prevmonth and prevyear are the month and year that correspond to the previous month on the calendar.
     var nextmonth;
     var prevmonth;
     var nextyear;
@@ -51,9 +63,15 @@ function calendarPage(month, year, day, wk_start_day) {
     prevyear = year;
     }
 
-
+    // empties the main container before building the html inside it.
     $('#main_container').html("");
 
+    // adds the top menu to the calendar
+    // the menu items are the button to go to the previous month,
+    // the label for the current month,
+    // the button to go to the next month,
+    // the button to toggle the preferences menu,
+    // and the button to toggle the addEvent menu.
     $('#main_container').append(`<div class = menu>
         <div class=menuitem onclick ='calendarPage(${prevmonth}, ${prevyear}, ${selected_day}, ${wk_start_day})' ><a>previous</a></div>
         <div class=menuitem><h1>${fullDate.toLocaleString('default', {month: 'long'})}, ${year}</h1></div>
@@ -62,6 +80,8 @@ function calendarPage(month, year, day, wk_start_day) {
         <div id = "pref-toggle" class = menuitem style = "float: right; margin-top: 30px;"><a>Preferences</a></div>
     </div>`)
 
+    // appends the preference options that can be toggled with the preference options button.
+    // the preference options allows the user to decide what day of the week they want their week to start on.
     $('#main_container').append(`<div id = "preference_options" class = "preference_options">
             <form method='POST' style = "display: block;"> 
                 <b style = "display: block;"><p>First Day of the Week:</p></b>
@@ -79,30 +99,69 @@ function calendarPage(month, year, day, wk_start_day) {
             </form>
         </div>`)
 
+    // configures the preference options button to toggle the preference options menu
     $('#pref-toggle').click(
         () => {
         $(".preference_options").toggle(); 
         }
     )
 
+    // adds a div for the add event menu
     $('#main_container').append(`<div id='add_event'></div>`);
+
     $('#add_event').append(`<div class = "add_event_options" style = "display: none;">
-<form method = "POST" style = "display: block;"> 
-    <div style = "display: block;">
-        <input type="text" name="new_event_name" placeholder="Event Name" class="name_input">
-    </div>
+    <form method = "POST" style = "display: block;"> 
+        <div style = "display: block;">
+            <input type="text" name="new_event_name" placeholder="Event Name" class="name_input">
+        </div>
 
-    <b style = "display: block;"><p>Please select how the date of your event will be calculated:</p></b>
-    <div>
-        <input type="radio" id="static_radio" name="event_type" value="static_day">
-        <label for="static_radio">By Month and Day (E.g. January 1st)</label> <br>
-      
-        <input type="radio" id="varied_radio" name="event_type" value="varied_day">
-        <label for="varied_radio">By Weekday and Month (E.g. 3rd Thursday of November)</label>
-    </div>
+        <b style = "display: block;"><p>Please select how the date of your event will be calculated:</p></b>
+        <div>
+            <input type="radio" id="static_radio" name="event_type" value="static_day">
+            <label for="static_radio">By Month and Day (E.g. January 1st)</label> <br>
+        
+            <input type="radio" id="varied_radio" name="event_type" value="varied_day">
+            <label for="varied_radio">By Weekday and Month (E.g. 3rd Thursday of November)</label>
+        </div>
 
-    <div class = "static_choices">
-        <select name="new_event_month"> 
+        <div class = "static_choices">
+            <select name="new_event_month"> 
+                <option value = 1>January</option>
+                <option value = 2>February</option>
+                <option value = 3>March</option>
+                <option value = 4>April</option>
+                <option value = 5>May</option>
+                <option value = 6>June</option>
+                <option value = 7>July</option>
+                <option value = 8>August</option>
+                <option value = 9>September</option>
+                <option value = 10>October</option>
+                <option value = 11>November</option>
+                <option value = 12>December</option>
+            </select>
+            <input type="text" name="new_event_day" placeholder="day">
+        </div>
+
+    <div class = "varied_day_choices">
+        <select name="new_event_weekdayofmonth"> 
+            <option value = 1>First</option>
+            <option value = 2>Second</option>
+            <option value = 3>Third</option>
+            <option value = 4>Fourth</option>
+            <option value = 5>Fifth</option>
+        </select>
+
+        <select name="new_event_weekday"> 
+            <option value = 0>Monday</option>
+            <option value = 1>Tuesday</option>
+            <option value = 2>Wednesday</option>
+            <option value = 3>Thursday</option>
+            <option value = 4>Friday</option>
+            <option value = 5>Saturday</option>
+            <option value = 6>Sunday</option>
+        </select>
+        <p style = "display: inline-flex; margin-inline-end: 5px; margin-inline-start: 5px;">of</p>
+        <select name="new_event_month_varied"> 
             <option value = 1>January</option>
             <option value = 2>February</option>
             <option value = 3>March</option>
@@ -117,57 +176,26 @@ function calendarPage(month, year, day, wk_start_day) {
             <option value = 12>December</option>
         </select>
 
-        <input type="text" name="new_event_day" placeholder="day">
     </div>
+    <input class= "submit_event" type="submit" value="Add Event">
+    </form>
+    </div>`);
 
-   <div class = "varied_day_choices">
-    <select name="new_event_weekdayofmonth"> 
-        <option value = 1>First</option>
-        <option value = 2>Second</option>
-        <option value = 3>Third</option>
-        <option value = 4>Fourth</option>
-        <option value = 5>Fifth</option>
-    </select>
 
-    <select name="new_event_weekday"> 
-        <option value = 0>Monday</option>
-        <option value = 1>Tuesday</option>
-        <option value = 2>Wednesday</option>
-        <option value = 3>Thursday</option>
-        <option value = 4>Friday</option>
-        <option value = 5>Saturday</option>
-        <option value = 6>Sunday</option>
-    </select>
-    <p style = "display: inline-flex; margin-inline-end: 5px; margin-inline-start: 5px;">of</p>
-    <select name="new_event_month"> 
-        <option value = 1>January</option>
-        <option value = 2>February</option>
-        <option value = 3>March</option>
-        <option value = 4>April</option>
-        <option value = 5>May</option>
-        <option value = 6>June</option>
-        <option value = 7>July</option>
-        <option value = 8>August</option>
-        <option value = 9>September</option>
-        <option value = 10>October</option>
-        <option value = 11>November</option>
-        <option value = 12>December</option>
-    </select>
-</div>
-<input class= "submit_event" type="submit" value="Add Event">
-</form>
-</div>`);
-    //reloadAddEvent();
-
+    // configures theadd event button to toggle the add event menu
     $('#add-toggle').click(
         () => {
         $(".add_event_options").toggle(); 
         }
     )
 
+    // adds the div for the day info
     $('#main_container').append(`<div id='day_info'></div>`);
+
+    // Puts the events linked to the selected day into the day_info div
     reloadDayInfo(month, year, day);
 
+    // adds the option to jump to a specific date to the calendar
     $('#main_container').append(`<form method='POST'> 
         <select name="month"> 
             <option value = 1>January</option>
@@ -189,26 +217,26 @@ function calendarPage(month, year, day, wk_start_day) {
         <input class= "submit-button" type="submit" value="Go To Date">
     </form>`)
 
- 
-
+    // adds the weekday labels above the calendar
     weekList.forEach((weekday_name) => {
         $('#main_container').append(`<div class="weekday">
             <div class="weekdaytxt">${weekday_name}</div>
             </div>`);
     });
 
-
+    // adds a day cell to the calendar for each day in the month there is.
     for (let i = 0; i < firstDayInMonthIndex; i += 1){
         $('#main_container').append(`<a class="day"></a>`);
     }
 
 
+    var moon_images; // the list of the images that correspond to the moon phase for each day of the month
+    var moon_img; // the image that corresponds to the moon phase for the current day
 
-
-
-    var moon_images;
-    var moon_img;
-
+    // adds the moon image to each day cell.
+    // links every day cell to an onclick function so that is a day is clicked,
+    // that day becomes the new selected day.
+    // Also makes the current selected day highlighted in a yellow border.
     $.ajax({
 
         url: `/api/moon_imgs_month/${month}/${year}/${lastDayInMonth}`
@@ -234,56 +262,6 @@ function calendarPage(month, year, day, wk_start_day) {
     })
 
 
-
-
-
-
-
-
-
-
-        // console.log(day);
-        // $(`#${day}`).className = 'selectedday';
-        //$(`#${day}`).classList.add('selectedday');
-
-
-
-        // $.ajax({
-        //     url: "/api/
-        //   }).done(function(data) {
-        //      // do something
-        //   });
-
-        // for i in range(1, (lastofmonth + 1)):
-        // day = datetime(year, month, i, 0, 0)
-        // dates.append((day, phase(position(day)), (position(day))))
-
-    
-
-
-
-    //     selected_day = new_day;
-    //     var new_day_element = document.getElementById(selected_day);
-    //     new_day_element.className = "selectedday";
-    // }
-
-    // function clickDay(date) {
-    //     var new_day = date.substring(8, 10);
-    //     changeDay(new_day);
-    // }
-
-    // function clickDayAjax(date){
-    //     //var new_day = $(this).attr(id);
-    //     var new_day = date.substring(8,10);
-    //     alert(new_day);
-    //     $.ajax({
-    //         url: "/?month=" + "{{ month }}" + "&day=" + new_day + "&year=" + "{{ year }}" + "&wk_start_day=" + "{{ wk_start_day }}"
-    //         //url: "{{ url_for('start', year = year, month = month, day = new_day, wk_start_day = wk_start_day)}}"
-    //     }).done(function() {
-    //         alert("done");
-    //     })
-    //}
-
     $('input[type=radio][name="event_type"][value="static_day"]').change(function() {
         $(".varied_day_choices").hide(); 
         $(".static_choices").show(); 
@@ -296,6 +274,10 @@ function calendarPage(month, year, day, wk_start_day) {
     }); 
 
 }
+
+// switches the selected_day to a new day in the same month and year.
+// the day cell of the current selected_day should retrun to a white border,
+// and the new selected_day should have a yellow border.
 function changeDay(month, year, new_day) {  
     var prev_day_element = document.getElementById(selected_day);
     var new_day_element = document.getElementById(new_day);
@@ -305,13 +287,14 @@ function changeDay(month, year, new_day) {
     new_day_element.className = "selectedday";
     prev_day_element_div.className = "day";
     new_day_element_div.className = "selectedday";
-
-
   
     selected_day = new_day;
+
+    // shows the event information for the new current day
     reloadDayInfo(month, year, new_day)
 }
 
+// displays the list of all the events that occur on the seleced_day
 function reloadDayInfo(month, year, day) {
     $('#day_info').html("");
     $.ajax({
@@ -323,112 +306,4 @@ function reloadDayInfo(month, year, day) {
         });
     });
 }
-
-// function reloadAddEvent() {
-//     $('#add_event').html("");
-//     $('#add_event').append(`<div class = "add_event_options" style = "display: none;">
-// <form method = "POST" style = "display: block;"> 
-//     <div style = "display: block;">
-//         <input type="text" name="new_event_name" placeholder="Event Name" class="name_input">
-//     </div>
-
-//     <b style = "display: block;"><p>Please select how the date of your event will be calculated:</p></b>
-//     <div>
-//         <input type="radio" id="static_radio" name="event_type" value="static_day">
-//         <label for="static_radio">By Month and Day (E.g. January 1st)</label> <br>
-      
-//         <input type="radio" id="varied_radio" name="event_type" value="varied_day">
-//         <label for="varied_radio">By Weekday and Month (E.g. 3rd Thursday of November)</label>
-//     </div>
-
-//     <div class = "static_choices">
-//         <select name="new_event_month"> 
-//             <option value = 1>January</option>
-//             <option value = 2>February</option>
-//             <option value = 3>March</option>
-//             <option value = 4>April</option>
-//             <option value = 5>May</option>
-//             <option value = 6>June</option>
-//             <option value = 7>July</option>
-//             <option value = 8>August</option>
-//             <option value = 9>September</option>
-//             <option value = 10>October</option>
-//             <option value = 11>November</option>
-//             <option value = 12>December</option>
-//         </select>
-
-//         <input type="text" name="new_event_day" placeholder="day">
-//     </div>
-
-//    <div class = "varied_day_choices">
-//     <select name="new_event_weekdayofmonth"> 
-//         <option value = 1>First</option>
-//         <option value = 2>Second</option>
-//         <option value = 3>Third</option>
-//         <option value = 4>Fourth</option>
-//         <option value = 5>Fifth</option>
-//     </select>
-
-//     <select name="new_event_weekday"> 
-//         <option value = 0>Monday</option>
-//         <option value = 1>Tuesday</option>
-//         <option value = 2>Wednesday</option>
-//         <option value = 3>Thursday</option>
-//         <option value = 4>Friday</option>
-//         <option value = 5>Saturday</option>
-//         <option value = 6>Sunday</option>
-//     </select>
-//     <p style = "display: inline-flex; margin-inline-end: 5px; margin-inline-start: 5px;">of</p>
-//     <select name="new_event_month"> 
-//         <option value = 1>January</option>
-//         <option value = 2>February</option>
-//         <option value = 3>March</option>
-//         <option value = 4>April</option>
-//         <option value = 5>May</option>
-//         <option value = 6>June</option>
-//         <option value = 7>July</option>
-//         <option value = 8>August</option>
-//         <option value = 9>September</option>
-//         <option value = 10>October</option>
-//         <option value = 11>November</option>
-//         <option value = 12>December</option>
-//     </select>
-// </div>
-// <input class= "submit_event" type="submit" value="Add Event">
-// </form>
-// </div>`);
-
-// }
-
- $(document).ready(function() {
-
-    // $('input[type=radio][name="event_type"][value="static_day"]').change(function() {
-    //     $(".varied_day_choices").hide(); 
-    //     $(".static_choices").show(); 
-    //     $(".submit_event").show(); 
-    // }); 
-    // $('input[type=radio][name="event_type"][value="varied_day"]').change(function() {
-    //     $(".varied_day_choices").show(); 
-    //     $(".static_choices").hide(); 
-    //     $(".submit_event").show(); 
-
-// $('#static_radio').click(
-//     () => {
-//         console.log("click static radio");
-//         $(".varied_day_choices").hide(); 
-//         $(".static_choices").show(); 
-//         $(".submit_event").show();  
-//     }
-// )
-
-// $('#varied_radio').click(
-//     () => {
-//         console.log("click varied radio");
-//         $(".varied_day_choices").show(); 
-//         $(".static_choices").hide(); 
-//         $(".submit_event").show();  
-//     }
-// )
- });
-//});
 
